b1be1299f48c4a6acde3efd5a596ce0aacc1b397
Mat Morgan <mamorga@microsoft.com>
2020-04-10 14:45:44 -0700

    fix my dumb git errors

--- prep-wvdImage-noinline.ps1
+++ prep-wvdImage-noinline.ps1
@@ -11,20 +11,21 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 $ErrorActionPreference = 'Continue'
 
-Install-Module -Name PowerShellGet -Repository PSGallery -Force -ErrorAction Stop
-Install-Module -Name Az -AllowClobber
+#Install-Module -Name PowerShellGet -Repository PSGallery -Force -ErrorAction Stop
+#Install-Module -Name Az -AllowClobber
 
 # Set this variable to your FSLogix profile directory
+mkdir "c:\buildArtifacts"; cd "c:\buildArtifacts"; New-Item "buildActionsOutput.txt" -itemType File
+#$logPath = "C:\buildArtifacts\buildActionsOutput.txt"
+Write-Log -Message "This script will prepare your image for capture and eventual upload to Azure."
 
-Write-Host "This script will prepare your image for capture and eventual upload to Azure."
-
-Write-Host "Disabling Automatic Updates..."
+Write-Log -Message "Disabling Automatic Updates..."
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 1 /f
 
-Write-Host "Moving pagefile.sys to D:\"
+Write-Log -Message "Moving pagefile.sys to D:\"
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name "PagingFiles" -Value "D:\pagefile.sys" -Type MultiString -Force
 
-#Write-Host "Setting OneDrive for Business policies" Run this after you install One Drive
+#Write-Log -Message "Setting OneDrive for Business policies" Run this after you install One Drive
 #Configure OneDrive to start at sign-in for all users
 #REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
 #Silently configure user accounts
@@ -33,6 +34,7 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\M
 #REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "bad69d79-XXXX-XXXX-9157-966cbd2d9933" /f
 
 # Enter the following commands into the registry editor to fix 5k resolution support
+Write-Log -Message "add 5k resolution support"
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v MaxMonitors /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v MaxXResolution /t REG_DWORD /d 5120 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v MaxYResolution /t REG_DWORD /d 2880 /f
@@ -41,21 +43,23 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\rdp-s
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\rdp-sxs" /v MaxYResolution /t REG_DWORD /d 2880 /f
 
 # Enable timezone redirection
-Write-Host "Enabling time zone redirection..."
+Write-Log -Message "Enabling time zone redirection..."
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableTimeZoneRedirection /t REG_DWORD /d 1 /f
 
 # Disable Storage Sense
-Write-Host "Disabling Storage Sense..."
+Write-Log -Message "Disabling Storage Sense..."
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy /v 01 /t REG_DWORD /d 0 /f
 
 # Remove the WinHTTP proxy
 #netsh winhttp reset proxy
 
 # Set Coordinated Universal Time (UTC) time for Windows and the startup type of the Windows Time (w32time) service to Automatically
+Write-Log -Message "set UTC and w32time"
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -name "RealTimeIsUniversal" -Value 1 -Type DWord -force
 Set-Service -Name w32time -StartupType Automatic
 
 # Set the power profile to the High Performance
+Write-Log -Message "set power scheme"
 powercfg /setactive SCHEME_MIN
 
 # Make sure that the environmental variables TEMP and TMP are set to their default values
@@ -72,7 +76,7 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\E
 #Set-Service -Name termService -StartupType Manual
 #Set-Service -Name RemoteRegistry -StartupType Automatic
 #Set-Service -Name Winrm -startuptype Automatic
-
+Write-Log -Message "set RDP and TS registry keys"
 # Ensure RDP is enabled
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0 -Type DWord -force
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDenyTSConnections" -Value 0 -Type DWord -force
@@ -105,6 +109,7 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\W
 # Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SSLCertificateSHA1Hash" -force
 
 # Turn on Firewall
+Write-Log -Message "enable firewall and allow winRM, RD, FP sharing"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 # Allow WinRM
@@ -123,10 +128,11 @@ Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv
 # Add-MpPreference -ExclusionPath $FSLUNC
 
 #Add FSLogix settings
+Write-Log -Message "set FSLogix settings"
 New-Item -Path HKLM:\Software\FSLogix\ -Name Profiles -Force
 New-Item -Path HKLM:\Software\FSLogix\Profiles\ -Name Apps -Force
 Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "Enabled" -Type "Dword" -Value "1"
-New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "VHDLocations" -Value "\\mmftaade2vm\PROFILE" -PropertyType MultiString -Force
+New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "VHDLocations" -Value \\mmftaade2vm\PROFILE -PropertyType MultiString -Force
 Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SizeInMBs" -Type "Dword" -Value "1024"
 Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "IsDynamic" -Type "Dword" -Value "1"
 Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "VolumeType" -Type String -Value "VHDx"
@@ -141,10 +147,85 @@ Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNamePattern"
 Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNameMatch" -Type String -Value "%username%%sid%"
 
 #add choco and test chrome install
+Write-Log -Message "install Choco and test chrome install"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install GoogleChrome -y
 
-
+Exit 0
 # Launch Sysprep - not needed if running in AIB
 # Write-Host "We'll now launch Sysprep."
 # C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown
+# add Write-Log function
+function Write-Log
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("LogContent")]
+        [string]$Message,
+
+        [Parameter(Mandatory=$false)]
+        [Alias('LogPath')]
+        [string]$Path='C:\buildArtifacts\buildActionsOutput.txt',
+        
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Error","Warn","Info")]
+        [string]$Level="Info",
+        
+        [Parameter(Mandatory=$false)]
+        [switch]$NoClobber
+    )
+
+    Begin
+    {
+        # Set VerbosePreference to Continue so that verbose messages are displayed.
+        $VerbosePreference = 'Continue'
+    }
+    Process
+    {
+        
+        # If the file already exists and NoClobber was specified, do not write to the log.
+        if ((Test-Path $Path) -AND $NoClobber) {
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name."
+            Return
+            }
+
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path.
+        elseif (!(Test-Path $Path)) {
+            Write-Verbose "Creating $Path."
+            $NewLogFile = New-Item $Path -Force -ItemType File
+            }
+
+        else {
+            # Nothing to see here yet.
+            }
+
+        # Format Date for our Log File
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+        # Write message to error, warning, or verbose pipeline and specify $LevelText
+        switch ($Level) {
+            'Error' {
+                Write-Error $Message
+                $LevelText = 'ERROR:'
+                }
+            'Warn' {
+                Write-Warning $Message
+                $LevelText = 'WARNING:'
+                }
+            'Info' {
+                Write-Verbose $Message
+                $LevelText = 'INFO:'
+                }
+            }
+        
+        # Write log entry to $Path
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append
+    }
+    End
+    {
+    }
+}
